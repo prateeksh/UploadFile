2b70171368695ac9282986876770beb9d446ad31
Prateek <sharma.prateek95@gmail.com>
2017-06-29 00:02:11 +0530

    initial

---
+++ src/main/java/com/example/UploadController.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UploadController {
+
+    private UploadRepository repository;
+
+    @Autowired
+    public UploadController(UploadRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UploadData> get(@PathVariable("id") Long id) {
+        UploadData user = repository.findOne(id);
+        if (null == user) {
+            return new ResponseEntity<UploadData>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<UploadData>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/addnew", method = RequestMethod.POST)
+    public ResponseEntity<UploadData> update(@RequestBody UploadData user) {
+        repository.save(user);
+        return get(user.getId());
+    }
+
+    @RequestMapping(value = "/result", method = RequestMethod.GET)
+    public List<UploadData> all() {
+        return repository.findAll();
+    }
+
+    @RequestMapping("/datacheck")
+    @ResponseBody
+    String data() {
+        return "check data";
+    }
+}
---
+++ src/main/java/com/example/UploadData.java
@@ -0,0 +1,82 @@
+package com.example;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "uploadfile")
+public class UploadData {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @NotEmpty
+    @Column(name = "teach_id")
+    private long teach_id;
+
+    @NotEmpty
+    @Column(name = "image")
+    private String image;
+
+    @NotEmpty
+    @Column(name = "video")
+    private String video;
+
+    @NotEmpty
+    @Column(name = "presentation")
+    private String presentation;
+
+
+    public UploadData() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getTeach_id() {
+        return teach_id;
+    }
+
+    public void setTeach_id(long teach_id) {
+        this.teach_id = teach_id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getPresentation() {
+        return presentation;
+    }
+
+    public void setPresentation(String presentation) {
+        this.presentation = presentation;
+    }
+}
+
